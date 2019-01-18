6bae8bba766b7ec5bca13ec5beb83247c63e4a74
nurali-techie <nurali.techie@gmail.com>
2019-01-18 17:42:49 +0530

    use actual config for config_error

--- controller/status_blackbox_test.go
+++ controller/status_blackbox_test.go
@@ -2,6 +2,8 @@ package controller_test
 
 import (
 	"errors"
+	"os"
+	"strconv"
 	"testing"
 	"time"
 
@@ -16,21 +18,13 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+const (
+	wantConfigErrMsgForDevMode  = "Error: developer Mode is enabled; default DB password is used; Sentry DSN is empty"
+	wantConfigErrMsgForProdMode = "Error: default DB password is used; Sentry DSN is empty; Auth service url is empty; Cluster service url is empty"
+)
+
 type StatusControllerSuite struct {
 	testsuite.DBTestSuite
-}
-
-type testStatusConfig struct {
-	devMode   bool
-	configErr error
-}
-
-func (s *testStatusConfig) DeveloperModeEnabled() bool {
-	return s.devMode
-}
-
-func (s *testStatusConfig) DefaultConfigError() error {
-	return s.configErr
 }
 
 type testDBChecker struct {
@@ -46,13 +40,17 @@ func TestStatusController(t *testing.T) {
 	suite.Run(t, &StatusControllerSuite{DBTestSuite: testsuite.NewDBTestSuite(config)})
 }
 
-func (s *StatusControllerSuite) UnSecuredController(config *testStatusConfig) (*goa.Service, *controller.StatusController) {
+func (s *StatusControllerSuite) UnSecuredController() (*goa.Service, *controller.StatusController) {
+	config, err := configuration.New("")
+	require.NoError(s.T(), err)
 	svc := goa.New("status-test")
 	ctrl := controller.NewStatusController(svc, controller.NewGormDBChecker(s.DBTestSuite.DB), config)
 	return svc, ctrl
 }
 
-func (s *StatusControllerSuite) UnSecuredControllerWithUnreachableDB(config *testStatusConfig) (*goa.Service, *controller.StatusController) {
+func (s *StatusControllerSuite) UnSecuredControllerWithUnreachableDB() (*goa.Service, *controller.StatusController) {
+	config, err := configuration.New("")
+	require.NoError(s.T(), err)
 	svc := goa.New("status-test")
 	ctrl := controller.NewStatusController(svc, &testDBChecker{}, config)
 	return svc, ctrl
@@ -61,61 +59,64 @@ func (s *StatusControllerSuite) UnSecuredControllerWithUnreachableDB(config *tes
 func (s *StatusControllerSuite) TestShow() {
 
 	s.T().Run("ok_with_dev_mode", func(t *testing.T) {
-		config := &testStatusConfig{
-			devMode:   true,
-			configErr: nil,
-		}
-		svc, ctrl := s.UnSecuredController(config)
+		currDevMode := os.Getenv("F8_DEVELOPER_MODE_ENABLED")
+		defer os.Setenv("F8_DEVELOPER_MODE_ENABLED", currDevMode)
+
+		wantDevMode := true
+		os.Setenv("F8_DEVELOPER_MODE_ENABLED", strconv.FormatBool(wantDevMode))
+		svc, ctrl := s.UnSecuredController()
 
 		_, got := test.ShowStatusOK(t, svc.Context, svc, ctrl)
 
 		checkStatus(t, got)
-		assert.Equal(t, true, *got.DevMode)
+		assert.Equal(t, wantDevMode, *got.DevMode)
 		assert.Equal(t, "OK", got.DatabaseStatus)
-		assert.Equal(t, "OK", got.ConfigurationStatus)
+		assert.Equal(t, wantConfigErrMsgForDevMode, got.ConfigurationStatus)
 	})
 
 	s.T().Run("ok_with_dev_mode_with_config_issue", func(t *testing.T) {
-		config := &testStatusConfig{
-			devMode:   true,
-			configErr: errors.New("configuration error"),
-		}
-		svc, ctrl := s.UnSecuredController(config)
+		currDevMode := os.Getenv("F8_DEVELOPER_MODE_ENABLED")
+		defer os.Setenv("F8_DEVELOPER_MODE_ENABLED", currDevMode)
+
+		wantDevMode := true
+		os.Setenv("F8_DEVELOPER_MODE_ENABLED", strconv.FormatBool(wantDevMode))
+		svc, ctrl := s.UnSecuredController()
 
 		_, got := test.ShowStatusOK(t, svc.Context, svc, ctrl)
 
 		checkStatus(t, got)
-		assert.Equal(t, true, *got.DevMode)
+		assert.Equal(t, wantDevMode, *got.DevMode)
 		assert.Equal(t, "OK", got.DatabaseStatus)
-		assert.Equal(t, "Error: configuration error", got.ConfigurationStatus)
+		assert.Equal(t, wantConfigErrMsgForDevMode, got.ConfigurationStatus)
 	})
 
 	s.T().Run("service_unavailable_with_config_issue", func(t *testing.T) {
-		config := &testStatusConfig{
-			devMode:   false,
-			configErr: errors.New("configuration error"),
-		}
-		svc, ctrl := s.UnSecuredController(config)
+		currDevMode := os.Getenv("F8_DEVELOPER_MODE_ENABLED")
+		defer os.Setenv("F8_DEVELOPER_MODE_ENABLED", currDevMode)
+
+		os.Setenv("F8_DEVELOPER_MODE_ENABLED", "false")
+		svc, ctrl := s.UnSecuredController()
 
 		_, got := test.ShowStatusServiceUnavailable(t, svc.Context, svc, ctrl)
 
 		assert.Nil(t, got.DevMode)
 		assert.Equal(t, "OK", got.DatabaseStatus)
-		assert.Equal(t, "Error: configuration error", got.ConfigurationStatus)
+		assert.Equal(t, wantConfigErrMsgForProdMode, got.ConfigurationStatus)
 	})
 
 	s.T().Run("service_unavailable_with_db_issue", func(t *testing.T) {
-		config := &testStatusConfig{
-			devMode:   true,
-			configErr: nil,
-		}
-		svc, ctrl := s.UnSecuredControllerWithUnreachableDB(config)
+		currDevMode := os.Getenv("F8_DEVELOPER_MODE_ENABLED")
+		defer os.Setenv("F8_DEVELOPER_MODE_ENABLED", currDevMode)
+
+		wantDevMode := true
+		os.Setenv("F8_DEVELOPER_MODE_ENABLED", strconv.FormatBool(wantDevMode))
+		svc, ctrl := s.UnSecuredControllerWithUnreachableDB()
 
 		_, got := test.ShowStatusServiceUnavailable(t, svc.Context, svc, ctrl)
 
-		assert.Equal(t, true, *got.DevMode)
+		assert.Equal(t, wantDevMode, *got.DevMode)
 		assert.Equal(t, "Error: DB is unreachable", got.DatabaseStatus)
-		assert.Equal(t, "OK", got.ConfigurationStatus)
+		assert.Equal(t, wantConfigErrMsgForDevMode, got.ConfigurationStatus)
 	})
 
 }
