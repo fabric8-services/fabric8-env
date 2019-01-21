dc8a4d7717a16efefa50e1c2f406d54fbe83a427
nurali-techie <nurali.techie@gmail.com>
2019-01-21 15:42:03 +0530

    test added

--- configuration/configuration_blackbox_test.go
+++ configuration/configuration_blackbox_test.go
@@ -46,3 +46,45 @@ func (s *ConfigurationTestSuite) TestAuthURL() {
 	require.NoError(s.T(), err)
 	assert.Equal(s.T(), "https://api.some.io", config.GetAuthServiceURL())
 }
+
+func (s *ConfigurationTestSuite) TestConfigErr() {
+	t := s.T()
+	currPgPass := os.Getenv("F8_POSTGRES_PASSWORD")
+	currSentryDSN := os.Getenv("F8_SENTRY_DSN")
+	currAuthURL := os.Getenv("F8_AUTH_URL")
+	currClusterURL := os.Getenv("F8_CLUSTER_URL")
+	defer func() {
+		os.Setenv("F8_POSTGRES_PASSWORD", currPgPass)
+		os.Setenv("F8_SENTRY_DSN", currSentryDSN)
+		os.Setenv("F8_AUTH_URL", currAuthURL)
+		os.Setenv("F8_CLUSTER_URL", currClusterURL)
+	}()
+
+	configErr := createConfigGetConfigErr(t)
+	assert.Equal(t, "default DB password is used; Sentry DSN is empty; Auth service url is empty; Cluster service url is empty", configErr.Error())
+
+	os.Setenv("F8_POSTGRES_PASSWORD", "abcd1234")
+	configErr = createConfigGetConfigErr(t)
+	assert.Equal(t, "Sentry DSN is empty; Auth service url is empty; Cluster service url is empty", configErr.Error())
+
+	os.Setenv("F8_SENTRY_DSN", "https://somedsn.com")
+	configErr = createConfigGetConfigErr(t)
+	assert.Equal(t, "Auth service url is empty; Cluster service url is empty", configErr.Error())
+
+	os.Setenv("F8_AUTH_URL", "https://someauth.com")
+	configErr = createConfigGetConfigErr(t)
+	assert.Equal(t, "Cluster service url is empty", configErr.Error())
+
+	os.Unsetenv("F8_AUTH_URL")
+	os.Setenv("F8_CLUSTER_URL", "https://somecluster.com")
+	configErr = createConfigGetConfigErr(t)
+	assert.Equal(t, "Auth service url is empty", configErr.Error())
+}
+
+func createConfigGetConfigErr(t *testing.T) error {
+	config, err := configuration.New("")
+	require.NoError(t, err)
+	configErr := config.DefaultConfigError()
+	assert.NotNil(t, configErr)
+	return configErr
+}
