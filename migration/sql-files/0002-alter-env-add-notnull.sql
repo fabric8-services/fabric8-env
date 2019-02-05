87f69ab6a7200592cf233d463567d097706f8bfb
nurali-techie <nurali.techie@gmail.com>
2019-02-05 15:16:03 +0530

    migrate existing null values with default

--- migration/sql-files/0002-alter-env-add-notnull.sql
+++ migration/sql-files/0002-alter-env-add-notnull.sql
@@ -1,3 +1,8 @@
+UPDATE environments SET name = 'dummy-env' WHERE name IS NULL OR name = '';
 ALTER TABLE environments ALTER COLUMN name SET NOT NULL;
+
+UPDATE environments SET type = 'stage' WHERE type IS NULL OR type = '';
 ALTER TABLE environments ALTER COLUMN type SET NOT NULL;
+
+UPDATE environments SET cluster_url = 'https://api.starter-us-east-2.openshift.com' WHERE cluster_url IS NULL OR cluster_url = '';
 ALTER TABLE environments ALTER COLUMN cluster_url SET NOT NULL;
